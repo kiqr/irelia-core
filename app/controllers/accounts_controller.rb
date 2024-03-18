5f46b1a3354bf97665b9e404d0a0221c6cfb40d7
Rasmus Kjellberg <2277443+kjellberg@users.noreply.github.com>
2024-03-18 14:08:28 +0100

    Move navigation views to app/views/navigations

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,7 +3,7 @@
 class AccountsController < Teamable::AccountsController
   def edit
     breadcrumb "Settings", "#"
-    breadcrumb "Edit account profile", edit_account_path
+    breadcrumb "Account profile", edit_account_path
     super
   end
 end
